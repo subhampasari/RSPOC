83081104e40fafd757007728f4b2136e04fdb2ec
Subham Pasari <sp.subham1995@gmail.com>
2020-07-25 16:33:42 +0530

    First commit. Connected to Redshift. Create table, insert into table, fetch from table and GET API working.

---
+++ src/main/java/com/example/RSPOC/CustomerController.java
@@ -0,0 +1,34 @@
+package com.example.RSPOC;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        String cust[] = new String[100];
+        int i=0;
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> cust[i] = customer.toString());
+
+
+        return cust[0];
+    }
+
+}
---
+++ src/test/java/com/example/RSPOC/RspocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RSPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RspocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
